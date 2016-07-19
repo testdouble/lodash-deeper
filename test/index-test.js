f9bb0cbb5ca03482e137aa510d3124d6e2a25fb7
Mathew Weitzel <mweitzel@gmail.com>
2016-07-19 10:22:09 -0400

    pre-refactor test rearrangement

--- test/index-test.js
+++ test/index-test.js
@@ -1,17 +1,17 @@
-var _ = require('../index')
+var lodash = require('../index')
 var assertEquals = require('./support/assert-equals')
 var nestedFilteredPair = require('./support/generate-nested-and-expected-filtered-object-pair')
 
 var sample
 var expected
-module.exports = {
+var tests = {
   beforeEach: function () {
     var pair = nestedFilteredPair()
     sample = pair.nestedObject
     expected = pair.filterResult
   },
   filtersStuff: function () {
-    var result = _.filterDeep(sample, ['color', 'red'])
+    var result = _.filterDeep(sample, predicate)
     assertEquals(result, expected)
   },
   uniqVisited: function () {
@@ -19,5 +19,17 @@ module.exports = {
     expected.push(sample)
     var result = _.filterDeep(sample, ['color', 'red'])
     assertEquals(result, expected)
+  }
+}
+
+var _
+var predicate
+module.exports = {
+  withLodashFull: {
+    beforeEach: function () {
+      _ = lodash
+      predicate = ['color', 'red']
+    }
+  , test: tests
   }
 }
